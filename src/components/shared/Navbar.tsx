73e0797e1ae7ca1485c59fd44a9e32655eec4d2b
Serhii <avatarcapa@gmail.com>
2026-01-08 00:33:11 +0100

    feat: Navbar component (basic implementation)

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Link from 'next/link';
+
+interface NavbarProps {
+  activePage: string;
+}
+
+export const NAV_URLS = {
+  home: '/',
+  watchlist: '/watchlist',
+  favorites: '/favorites',
+};
+
+const Navbar: React.FC<NavbarProps> = ({ activePage }) => {
+  return (
+    <header className="border-border bg-background-input dark:border-border-inverse dark:bg-background-neutral sticky top-0 z-50 w-full border-b">
+      <div className="mx-auto flex w-full max-w-[1440px] items-center justify-between px-200 py-150 md:px-500">
+        <Link
+          href={NAV_URLS.home}
+          className="text-color-text dark:text-text-inverse flex cursor-pointer items-center gap-200"
+        >
+          <h2 className="font-font-weight-bold text-lg leading-tight tracking-[-0.015em]">
+            TV-Show Explorer
+          </h2>
+        </Link>
+        <div className="hidden flex-1 items-center justify-end gap-400 md:flex">
+          <nav className="flex h-9 items-center gap-300">
+            <Link
+              href={NAV_URLS.watchlist}
+              className={`${activePage === NAV_URLS.watchlist ? 'text-text-brand dark:text-text-brand' : 'text-text dark:text-text-inverse'} hover:text-text-brand dark:hover:text-icon-accent-blue text-sm font-medium transition-colors`}
+            >
+              My Watchlist
+            </Link>
+            <Link
+              href={NAV_URLS.favorites}
+              className={`${activePage === NAV_URLS.favorites ? 'text-text-brand dark:text-text-brand' : 'text-text dark:text-text-inverse'} hover:text-text-brand dark:hover:text-icon-accent-blue text-sm font-medium transition-colors`}
+            >
+              My Favourites
+            </Link>
+          </nav>
+        </div>
+        <button className="text-text dark:text-text-inverse md:hidden">
+          <span className="material-symbols-outlined">menu</span>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
