c0067ca164e4e6cc6de1b76d840d8785bf9b4dfa
Serhii <avatarcapa@gmail.com>
2026-01-07 15:34:05 +0100

    feat: API client to get data from TVMaze API service - included next fetch fullUrl logging for dev mode

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   turbopack: {},
   webpack: (config, { defaultLoaders, isServer }) => {
     config.module.rules.push({
