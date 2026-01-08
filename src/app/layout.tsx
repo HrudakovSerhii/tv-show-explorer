69ad03493f3fadbe1a580b27362e418eb54c3c3a
Serhii <avatarcapa@gmail.com>
2026-01-08 03:25:31 +0100

    feat: Composed home page - Search for tv-show feature connected - Layout adjusted to render desktop/mobile UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,12 +26,15 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           rel="stylesheet"
         />
+        <title>TV Show Explorer</title>
       </head>
       <body className="antialiased">
         <AtlaskitThemeProvider />
         <div className="relative flex min-h-screen w-full flex-col overflow-x-hidden">
           <Navbar />
-          <main className="flex w-full flex-1 flex-col items-center">{children}</main>
+          <main className="flex w-full flex-1 flex-col items-center">
+            <div className="mx-auto w-full max-w-[1200px] px-200 py-300 md:px-500">{children}</div>
+          </main>
           <Footer />
         </div>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,22 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import React from 'react';
+
+import HeroSearch from '@/components/shared/HeroSearch';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between px-16 py-32 sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{' '}
-            <Link
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </Link>{' '}
-            or the{' '}
-            <Link
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="text-ds-link hover:text-ds-link-pressed font-medium"
-            >
-              Learning
-            </Link>{' '}
-            center.
-          </p>
+    <div className="flex flex-col gap-600">
+      <HeroSearch />
+
+      <section className="flex flex-col gap-200">
+        <div className="flex items-center justify-between">
+          <h2 className="text-text dark:text-text-inverse text-2xl leading-tight font-bold tracking-tight">
+            Web/Streaming Schedule
+          </h2>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <Link
-            className="bg-ds-background-brand-bold text-ds-text-inverse hover:bg-ds-background-brand-boldest flex h-12 w-full items-center justify-center gap-2 rounded-full px-5 transition-colors md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </Link>
-          <Link
-            className="border-ds-border hover:bg-ds-background-neutral-hovered flex h-12 w-full items-center justify-center rounded-full border border-solid px-5 transition-colors md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </Link>
+        <div className="grid grid-cols-2 gap-300 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+          {/*Streaming Schedule list*/}
         </div>
-      </main>
+      </section>
     </div>
   );
 }
