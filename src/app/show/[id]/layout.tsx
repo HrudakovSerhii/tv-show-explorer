efe3955c836c1d41dde95e1be1c1e256b3e1c005
Serhii <avatarcapa@gmail.com>
2026-01-09 01:38:44 +0100

    feat: Improved back navigation from episode details page - Episodes dropdown select state now use searchParams to select season when navigating back from episode page - Back navigation became Breadcrumb-like view with change per page
    chore: after Episode type updated we no longer need to check _links.show.name for undefined

--- src/app/show/[id]/layout.tsx
+++ src/app/show/[id]/layout.tsx
@@ -10,16 +10,6 @@ export type ShowLayoutProps = {
 export default function ShowLayout({ children }: ShowLayoutProps) {
   return (
     <div className="animate-fade-in mx-auto flex w-full max-w-[1024px] flex-col">
-      <nav className="flex flex-wrap items-center gap-100 px-200 pb-300 text-sm">
-        <Link
-          href={NAV_URLS.home}
-          className="text-brand font-weight-medium flex items-center gap-50 text-sm hover:underline"
-        >
-          <span className="material-symbols-outlined text-sm">arrow_back</span> Back to Home
-        </Link>
-        {/*TODO: Include Breadcrumbs navigation items after client state provider implementation*/}
-      </nav>
-
       <main className="flex flex-wrap items-center gap-100">{children}</main>
     </div>
   );
